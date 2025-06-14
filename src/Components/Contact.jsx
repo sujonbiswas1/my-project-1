84b71a654e71fa5acc15e1452c5705feb7110e42
sujonbiswas212 <wwwsujonbiswas212@gmail.com>
2025-06-14 08:32:21 +0600

    sujon

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,40 @@
-import w1 from '../images/w1.png'
-const Contact = () => {
+import React from 'react';
+import { toast } from 'react-toastify';
+import w1 from '../images/w1.png';
+
+ const Contact = () => {
+
+  const notify = () => toast("your information submit");
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "428fc45c-f4a5-42e6-a5f7-c54a1bfc2891");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success()
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult();
+      toast.error(data.message)
+    }
+  };
+
+
   return (
-    <div className="pb-50 pt-14 bg-black relative overflow-hidden"> 
+    <div className="md:pb-30 pb-7  md:-pt-18 bg-black relative overflow-hidden"> 
       <div className="px-4 sm:px-8 md:px-14 lg:px-32 container mx-auto">
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -13,10 +46,14 @@ const Contact = () => {
         <div>
           <h2 className="text-4xl text-center font-bold text-white mb-12">Contact Us</h2>
 
+
+          <form action="" onSubmit={onSubmit}>
+
+            
           <div className="bg-gradient-to-r p-1 from-blue-600 to-pink-500 rounded-3xl mb-12">
 
             <div className="bg-black p-2 rounded-3xl">
-              <input type="text" className="bg-transparent w-full text-white px-4 " />
+              <input type="text" name='email' className="bg-transparent w-full text-white px-4 " />
 
             </div>
 
@@ -27,13 +64,18 @@ const Contact = () => {
 
 
             <div className="bg-black p-2 rounded-3xl">
-              <textarea name="" id="" className="bg-transparent w-full text-white px-4 h-[10rem]"></textarea>
+              <textarea name="description" id="" className="bg-transparent w-full text-white px-4 h-[10rem]"></textarea>
 
             </div>
 
           </div>
 
-          <button className="w-full bg-linear-to-l from-pink-500 to-blue-500 text-white text-2xl rounded-3xl py-1 cursor-pointer">submit</button>
+          <button type='submit' onClick={notify} className="w-full bg-linear-to-l from-pink-500 to-blue-500 text-white text-2xl rounded-3xl py-2 cursor-pointer">submit</button>
+          <span>{result}</span>
+
+
+          </form>
+
 
         </div>
 
--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,17 +1,22 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Footer from "../Layers/Footer"
 import About from "./About"
 import Accelerator from "./Accelerator"
 import Blog from "./Blog"
+import Contact from "./Contact"
 import Hero from "./Hero"
 import Web from "./Web"
 const Index = () => {
   return (
     <div className="flex flex-col overflow-x-hidden">
+    <ToastContainer />
      <Hero />
      <Web />
      <About />
      <Accelerator />
      <Blog />
+     <Contact />
      <Footer />
    
  
